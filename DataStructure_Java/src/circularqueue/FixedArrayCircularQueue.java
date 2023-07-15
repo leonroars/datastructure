c2a3c99bd44f34fb2e33532b19cc937153b3a438
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2023-07-15 15:03:26 +0900

    close #2 add: Added FixedArrayCircularQueue

---
+++ DataStructure_Java/src/circularqueue/FixedArrayCircularQueue.java
@@ -0,0 +1,119 @@
+package circularqueue;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class FixedArrayCircularQueue<Item> implements Iterable<Item> {
+    Item[] faq;
+    private int frontP;
+    private int rearP;
+    private int currentCap;
+
+    // Setting constructor.
+    @SuppressWarnings("unchecked")
+    public FixedArrayCircularQueue(int initCapacity){
+        this.faq = (Item[])new Object[initCapacity];
+        this.currentCap = initCapacity;
+        this.frontP = 0;
+        this.rearP = 0;
+    }
+
+    // Convention:
+    //      A full-status is set to be 1-less than full capacity in fixed-capacity circular queue.
+    //      This is on purpose for 2-reasons.
+    //          - For discrimination of full-status and empty-status.
+    //          - For avoiding overflow.
+    public boolean isEmpty(){return this.frontP == this.rearP;}
+    public boolean isFull(){return this.size() == this.currentCap - 1;}
+
+    public int size(){return ((this.rearP - this.frontP) + this.currentCap) % this.currentCap;}
+
+    public void enqueue(Item item){
+        if(!this.isFull()){
+            this.faq[this.rearP] = item;
+            System.out.printf("enqueue() has been successfully processed!: %s\n", this.faq[this.rearP]);
+            this.rearP = (this.rearP + 1) % this.currentCap;
+        }else{
+            System.out.println("Circular Queue is now FULL!\n");
+        }
+    }
+
+    public Item dequeue(){
+        if(!this.isEmpty()){
+            Item dqedItem = this.faq[this.frontP];
+            System.out.printf("dequeue() has been successfully processed!: %s\n", dqedItem);
+            this.frontP++;
+            return dqedItem;
+        }else{
+            throw new NoSuchElementException("Circular Queue is now empty!");
+        }
+    }
+
+    public Item peek(){
+        if(!this.isEmpty()){
+            return this.faq[this.frontP];
+        }else{
+            throw new NoSuchElementException("Circular Queue is now empty!");
+        }
+    }
+
+    public Iterator<Item> iterator() {return new CircularQIterator(this.frontP, this.rearP, this.faq, this.size(), this.currentCap);}
+
+    public class CircularQIterator implements Iterator<Item>{
+        private int frontCounter;
+        private int rearCounter;
+        private Item[] cqReciever;
+        private int elemCounter;
+        private int qsize;
+        private int qcap;
+
+        public CircularQIterator(int frontPointer, int rearPointer, Item[] cq, int qsize, int qcap){
+            this.frontCounter = frontPointer;
+            this.rearCounter = rearPointer;
+            this.cqReciever = cq;
+            this.elemCounter = 0;
+            this.qsize = qsize;
+            this.qcap = qcap;
+        }
+
+        public boolean hasNext(){return this.elemCounter <= this.qsize;}
+
+        public Item next(){
+            Item nextItem = this.cqReciever[this.frontCounter];
+            this.frontCounter = (this.frontCounter + 1) % this.qcap;
+            this.elemCounter++;
+            return nextItem;
+        }
+        public void remove(){}
+    }
+
+    // Test code
+    public static void main(String[] args){
+        FixedArrayCircularQueue<String> faq1 = new FixedArrayCircularQueue<>(5);
+        //enqueue(), isEmpty(), isFull(), size(), peek(), pointers movement test.
+        faq1.enqueue("0");
+        faq1.enqueue("1");
+        faq1.enqueue("2");
+        faq1.enqueue("3");
+        System.out.printf("Current frontP: %d\n", faq1.frontP);
+        System.out.printf("Current rearP: %d\n", faq1.rearP);
+        System.out.printf("Current size(): %d\n", faq1.size());
+        System.out.printf("Currently Full?: %b\n", faq1.isFull());
+        System.out.printf("Currently Empty?: %b\n", faq1.isEmpty());
+        System.out.printf("Scheduled to be dequeue() next time: %s\n", faq1.peek());
+        System.out.printf("\n");
+
+        //dequeue(), pointers' circular movement test
+        faq1.dequeue();
+        faq1.dequeue();
+        faq1.enqueue("4");
+        faq1.enqueue("5");
+        System.out.printf("Current frontP: %d\n", faq1.frontP);
+        System.out.printf("Current rearP: %d\n", faq1.rearP);
+        System.out.printf("Current size(): %d\n", faq1.size());
+        System.out.printf("Currently Full?: %b\n", faq1.isFull());
+        System.out.printf("Currently Empty?: %b\n", faq1.isEmpty());
+        System.out.printf("Scheduled to be dequeue() next time: %s\n", faq1.peek());
+
+    }
+
+}
